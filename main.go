71a6af41a7d743a004751d3355ce1d804e66517f
Rus Hughes <russell.hughes@gmail.com>
2018-10-21 12:09:04 +0900

    s4 l39

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+
+	var rus person
+	rus.firstName = "Rus"
+	rus.lastName = "Hughes"
+
+	fmt.Printf("%+v", rus)
+}
