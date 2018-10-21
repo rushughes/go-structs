e7ec4e814473865e17778587f2e5efa3ec7a8985
Rus Hughes <russell.hughes@gmail.com>
2018-10-21 14:11:21 +0900

    s5 l49

--- maps.go
+++ maps.go
@@ -10,4 +10,16 @@ func main() {
 	}
 
 	fmt.Println(colors)
+
+	var colors2 map[string]string
+
+	fmt.Println(colors2)
+
+	colors3 := make(map[string]string)
+
+	colors3["white"] = "#ffffff"
+
+	delete(colors3, "white")
+
+	fmt.Println(colors3)
 }
